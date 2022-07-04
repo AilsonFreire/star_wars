e0aa44aa67294f23c0c934d0d391e62e8ca7b194
Ailson Freire <ailson_freire@outlook.com>
2022-07-04 10:23:50 +0200

    Fixing the SelectorCard component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/main";
+import Main from "./pages/main/Main";
 
 export const Router = () => (
 	<BrowserRouter>
